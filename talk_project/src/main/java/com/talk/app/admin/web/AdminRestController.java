0483ab3234a2ea7e840746cb0d198f6048ed2d04
podotang <  >
2024-08-05 08:15:56 +0900

    qwer

--- talk_project/src/main/java/com/talk/app/admin/web/AdminRestController.java
+++ talk_project/src/main/java/com/talk/app/admin/web/AdminRestController.java
@@ -25,7 +25,7 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/samplerest")
 //@RequiredArgsConstructor
 public class AdminRestController {
-	//test
+	//test   
     @Autowired
     private SampleService sampleService;
     
