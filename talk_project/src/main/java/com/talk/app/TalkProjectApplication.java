4350ea5c32bc77abdf99e89a2760850a9eceea25
MaengSeonWoo <msw0808@naver.com>
2024-08-05 12:10:21 +0900

    1212

--- talk_project/src/main/java/com/talk/app/TalkProjectApplication.java
+++ talk_project/src/main/java/com/talk/app/TalkProjectApplication.java
@@ -3,7 +3,7 @@ package com.talk.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.talk.app.**.mapper"})
 public class TalkProjectApplication {
 
 	public static void main(String[] args) {
