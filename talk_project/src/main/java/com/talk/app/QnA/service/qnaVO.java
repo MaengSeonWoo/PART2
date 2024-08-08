63db3f0c308d9c1dd0cb935aa6e008d40cb7364a
LuciMer25 <1997@seokwon.com>
2024-08-08 18:08:49 +0900

    ㅁㅁ

---
+++ talk_project/src/main/java/com/talk/app/QnA/service/qnaVO.java
@@ -0,0 +1,19 @@
+package com.talk.app.qna.service;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class qnaVO {
+	private Integer qnaNo; // 글 번호 
+	private String qnaTitle; // 제목
+	private String qnaContent; // 내용
+	@DateTimeFormat(pattern="yyyy/MM/dd")
+	private Date regDate; // 작성일
+	private String qnaState; // 답변상태
+	private int userNo; // 회원번호
+	private int coUserNo; // 기업회원번호
+}
