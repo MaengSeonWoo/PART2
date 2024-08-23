d2d7b69d50ee14c5d5c6534fc1bc25150af3dc75
MaengSeonWoo <msw0808@naver.com>
2024-08-23 11:26:43 +0900

    Merge branch 'master' into Maeng

---
+++ talk_project/src/main/java/com/talk/app/login/web/CustomAccessDeniedHandler.java
@@ -0,0 +1,22 @@
+package com.talk.app.login.web;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
+        ModelAndView modelAndView = new ModelAndView("/accessdenied");
+        modelAndView.addObject("errorMessage", "Access Denied");
+        response.sendRedirect("/accessdenied");
+    }
+}
