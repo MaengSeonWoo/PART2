6cd6a12a8f9955354c009f768146b1eaa4b44c2a
podotang <syj6180@naver.com>
2024-08-26 19:08:32 +0900

    아ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- talk_project/src/main/java/com/talk/app/admin/service/EmailService.java
+++ talk_project/src/main/java/com/talk/app/admin/service/EmailService.java
@@ -9,15 +9,16 @@ import com.talk.app.admin.mapper.WelfareMapper;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
 
 
 @Service
 public class EmailService {
 	@Value("${mailgun.api.key}")
-    private String apiKey;
+    private static String apiKey;
 
 	  @Value("${mailgun.domain.name}")
-	    private String domainName;
+	    private static String domainName;
 	  
 	  
     @Autowired
@@ -36,10 +37,8 @@ public class EmailService {
                         .field("subject", "Hello")
                         .field("text", "Testing Mailgun API")
                         .asJson();
-                
                 System.out.println("Response Status: " + request.getStatus());
                 System.out.println("Response Body: " + request.getBody().toString());
-
                 System.out.println("Email sent to: " + to);
                 System.out.println(request.getBody());
             } catch (Exception e) {
@@ -49,5 +48,21 @@ public class EmailService {
             System.out.println("No email address found for coUserNo: " + coUserNo);
         }
     }
+    
+    
+  	public JsonNode sendEmail() throws UnirestException {
+  		HttpResponse<JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + "sandbox3c725b0cf1e14d22b2b01773ce3c9fe1.mailgun.org" + "/messages")
+  	  			.basicAuth("api", "2b91eb47-df0104d2")
+  	  			.queryString("from", "Excited User <syj6180@naver.com>")
+  	  			.queryString("to", "syj6180@naver.com")
+  	  			.queryString("subject", "hello")
+  	  			.queryString("text", "testing")
+  	  			.asJson();
+  	  		return request.getBody();
+  	}
+    
+    
+    
+    
 
 }
