0a78eb4a809aa8f2fd81772a5b9e7a1428908138
Jinhyoung Kim <jinhyoung0213@gmail.com>
2024-08-30 17:36:24 +0900

    20240830 upload path

--- talk_project/src/main/java/com/talk/app/common/config/WebMvcConfig.java
+++ talk_project/src/main/java/com/talk/app/common/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 	
-	@Value("${file.upload.path}")
+	@Value("${file.upload.url.path}")
 	private String uploadPath;
 	
 	@Override
